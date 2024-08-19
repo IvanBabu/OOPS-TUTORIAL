43330a5f452459da5e7d98051acc500c5ef97a10
IvanBabu <ivanbabu1@gmail.com>
2024-08-19 10:33:45 +0530

    Add files via upload

---
+++ multtable.java
@@ -0,0 +1,17 @@
+import java.util*;
+class multtable
+{
+	public static void main(String args[])
+	{
+		int i,j;
+		System.out.println("The multiplication table is:");
+		for(i=1;i<=10;i++)
+		{
+			System.out.println("Table of"+i+":\n");
+			for(j=1;j<=10;j++)
+			{
+				System.out.println((i*j)+"\t");
+			}
+		}
+	}
+}	
